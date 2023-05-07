704223b8c513f6b8a7e65969411241b9bd3218fd
BruceBrAlva <111378956+BruceBrAlva@users.noreply.github.com>
2023-05-07 00:09:01 -0300

    Create Kart.java

---
+++ Kart.java
@@ -0,0 +1,17 @@
+package Principal;
+import java.util.Scanner;
+public class Kart {
+    Motor motor1 = new Motor();
+    Piloto piloto1 = new Piloto();
+    String nome = "Flash";
+    Scanner sc = new Scanner(System.in);
+    void fazerDrift(){
+        System.out.println("Parabéns Dominik Toreto, você Driftou");
+    }
+    void soltarTurbo(){
+        System.out.println("Você usou o Nitro e soltou um turbo!");
+    }
+    void pular(){
+        System.out.println("Você pula pela rampa e sai voando!!");
+    }
+}
